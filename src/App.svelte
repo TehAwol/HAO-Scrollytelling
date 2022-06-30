5ecf6437df8a54ee2c55d47f499e9bacddb3911a
Mikkel Vestergaard <56039404+vestermikk@users.noreply.github.com>
2022-06-30 20:36:56 +0200

    Implemented scroller component

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,70 @@
 <script>
+	import { onMount } from 'svelte';
 	import Map from './_components/Map.svelte';
+	import Scroller from '@sveltejs/svelte-scroller/Scroller.svelte';
+
+	let index, offset, progress;
+
+	let mapComponent;
+	function handleClick() {
+		console.log(index)
+		console.log(scrollyConfig.map[index])
+	}
+
+	const scrollyConfig = {
+		map: {
+			0: {
+				translate: [0, 0],
+				scale: 1,
+				dimension: "SCORE"
+			},
+			1: {
+				translate: [-7425.200238124638,-2003.7844077329937],
+				scale: 12.74624154056931,
+				dimension: "P1"
+			},
+			2: {
+				translate: [-6462.515760714943,-1449.3224019238412],
+				scale: 12.746241540569306,
+				dimension: "P3"
+			},
+			3: {
+				translate: [-7425.200238124638,-2003.7844077329937],
+				scale: 12.74624154056931,
+				dimension: "P2"
+			},
+		}
+	}
+
+	onMount(() => {
+		console.log(mapComponent)
+	})
+
+	// $: mapComponent.tweenSteps(scrollyConfig.map[index].scale, scrollyConfig.map[index].translate);
 
 </script>
 
 <h1>HAO Scrollytelling</h1>
-<Map></Map>
+<button on:click={handleClick}>Test</button>
+<Scroller top="{0.2}" bottom="{0.8}" bind:index bind:offset bind:progress>
+	<div slot="background">
+		<Map bind:this={mapComponent} debug={false} nextStep={scrollyConfig.map[index]}/>
+	</div>
+	<div slot="foreground">
+		<section>First section</section>
+		<section>Second section</section>
+		<section>Third s ection</section>
+		<section>Third s ection</section>
+	</div>
+
+</Scroller>
+
+<style>
+	section {
+		height: 80vh;
+		background-color: rgba(0,0,0,0.2);
+		color: white;
+		padding: 1em;
+		margin: 0 0 2em 0;
+	}
+</style>
--- src/_components/Map.svelte
+++ src/_components/Map.svelte
@@ -1,36 +1,34 @@
 <script>
     import { onMount } from 'svelte';
     import { feature } from 'topojson-client';
-    import { geoPath, geoAlbers, geoMercator} from 'd3-geo';
+    import { geoPath, geoMercator} from 'd3-geo';
 
     import worldTopo from '../_data/countries-50m.json';
     import accessData from '../_data/access-data.json';
+
+    import * as d3 from 'd3';
+
+    export let nextStep = {
+        translate: [0,0],
+        scale: 1
+    };
+
+    // Updates map whenever a newStep is received
+    $: tweenSteps(nextStep.scale, nextStep.translate);
+    // Updates dimension whenever a newStep is received
+    $: updateDimension(nextStep.dimension)
     
     const projection = geoMercator().scale(120);
     const path = geoPath().projection(projection);
 
     const worldjson = feature(worldTopo, worldTopo.objects.countries).features;
 
-    const colorSteps = ["#D9D9D9", "#FFD76D", "#FEA649", "#F86C30", "#E62F21", "#991824"];
     const pillarSteps = {
-        SCORE: ["#D9D9D9", "#FFD76D", "#FEA649", "#F86C30", "#E62F21", "#991824"],
-        P1: ["#D9D9D9", "#E1F2F6", "#C8DFE8", "#7FB1D4", "#3E78B3", "#133267"],
-        P2: ["#D9D9D9", "#E1EFD3", "#CBE8B8", "#95C987", "#4A9351", "#1A4220"],
-        P3: ["#D9D9D9", "#E0D3D9", "#CC9EAF", "#A3405C", "#752345", "#351229"]
+        SCORE: ["#FFFFFF", "#FFD76D", "#FEA649", "#F86C30", "#E62F21", "#991824"],
+        P1: ["#FFFFFF", "#E1F2F6", "#C8DFE8", "#7FB1D4", "#3E78B3", "#133267"],
+        P2: ["#FFFFFF", "#E1EFD3", "#CBE8B8", "#95C987", "#4A9351", "#1A4220"],
+        P3: ["#FFFFFF", "#E0D3D9", "#CC9EAF", "#A3405C", "#752345", "#351229"]
     };
-    
-
-    function findColor(feature) {
-        let color;
-        let name = feature.properties.name;
-        let score = accessData.find(el => el.Country === name);
-        if (score === undefined) {
-            color = "#D9D9D9";
-        } else {
-            color = colorSteps[score.SCORE];
-        }
-        return color;
-    }
 
     function addScoreClasses(feature) {
         let className;
@@ -48,6 +46,7 @@
         return className;
     }
 
+    // Applies accessData country name to country path
     function addCountryID(feature) {
         let id;
         let name = feature.properties.name;
@@ -60,7 +59,11 @@
         return id;
     }
 
-    function updateDimension(dimension) {
+    /**
+     * Updates ranked countries to given dimension colour gradient
+     * @param {string} dimension
+     */
+    export function updateDimension(dimension) {
         let steps = pillarSteps[dimension];
         let elements = document.getElementsByClassName("Ranked");
         for (let path of elements) {
@@ -70,26 +73,57 @@
         }
     }
 
-    worldjson.forEach(el => addScoreClasses(el));
+    // Zoom init
+    export let debug = false;
+
+    let zoom = d3.zoom().on('zoom', handleZoom);
+
+    function initZoom() {
+        d3.select('.map-container svg').call(zoom);
+    }
+
+    function handleZoom(e) {
+    d3.select("svg g")
+    .attr('transform', e.transform);
+    }
+
+    /**
+     * Transitions from current transform to given new scale and translation
+     * @param {number} scale 
+     * @param {Array} translate [x,y]
+     */
+    export function tweenSteps(scale, translate) {
+        d3.select("svg g")
+         .transition()
+         .duration(1000)
+         .call(zoom.transform, d3.zoomIdentity.translate(translate[0], translate[1]).scale(scale));
+    }
 
     onMount(() => {
-        console.log('Map component mounted')
-    }); 
+        console.log('Map component mounted');
+        worldjson.forEach(el => addScoreClasses(el));
+        updateDimension("SCORE");
+        if (debug) initZoom();
+    });
 
+    let tweenTranslate = [-6462.515760714943,-1449.3224019238412];
+    let tweenScale = 12.746241540569306;
+    
 </script>
 
 <button on:click={() => updateDimension("SCORE")}>Update Access</button>
 <button on:click={() => updateDimension("P1")}>Update P1</button>
 <button on:click={() => updateDimension("P2")}>Update P2</button>
 <button on:click={() => updateDimension("P3")}>Update P3</button>
+<button on:click={() => tweenSteps(tweenScale, tweenTranslate)}>tweenStep</button>
+<button on:click={() => tweenSteps(12.74624154056931, [-7425.200238124638,-2003.7844077329937])}>tweenStep</button>
 
 <div class="map-container">
     <svg viewBox="0 0 975 450">
-        <g fill="white" stroke="black" stroke-width=".5px">
+        <g fill="white" stroke="black" stroke-width=".25px">
             {#each worldjson as feature, i}
             <path 
                 d={path(feature)}
-                fill={findColor(feature)}
                 class={addScoreClasses(feature)}
                 id={addCountryID(feature)}
             />
